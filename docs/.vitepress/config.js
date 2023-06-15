8ff5cfdd16e8527d5fbadae3f706b92a3e3fffc2
lzh <277597509@qq.com>
2023-06-15 22:30:38 +0800

    init

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,25 +2,64 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "Frontend_About",
+  title: "Frontend About",
   description: "Frontend ",
   base: '/Frontend_About/',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: '首页', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: '前端', link: '/frontend/html', activeMatch: '^/frontend/' },
+      { text: '书籍', link: '/book/', activeMatch: '^/book/'},
+      { text: '面试', link: '/interview/html', activeMatch: '^/interview/'}
+      // { text: '随笔', link: '/article/', activeMatch: '^/article/'}
     ],
 
-    sidebar: [
-      {
-        text: 'Examples',
-        items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
-        ]
-      }
-    ],
+    sidebar: {
+      '/frontend/': [
+        {
+          items: [
+            { text: 'html', link: '/frontend/html' },
+            { text: 'css', link: '/frontend/css' },
+            { text: 'js', link: '/frontend/js' }
+          ]
+        }
+      ],
+      '/book/': [
+        {
+          items: [
+            { text: 'Markdown Examples', link: '/book/' },
+            { text: 'Runtime API Examples', link: '/book/book1' }
+          ]
+        }
+      ],
+      '/interview/': [
+        {
+          items: [
+            { text: 'html', link: '/interview/html' },
+            { text: 'css', link: '/interview/css' },
+            { text: 'js', link: '/interview/js' }
+          ]
+        }
+      ]
+    },
+
+    // sidebar: [
+    //   {
+    //     text: 'Examples',
+    //     items: [
+    //       { text: 'Markdown Examples', link: '/markdown-examples' },
+    //       { text: 'Runtime API Examples', link: '/api-examples' }
+    //     ]
+    //   },
+    //   {
+    //     text: '书籍',
+    //     items: [
+    //       { text: 'Markdown Examples', link: '/markdown-examples' },
+    //       { text: 'Runtime API Examples', link: '/api-examples' }
+    //     ]
+    //   }
+    // ],
 
     socialLinks: [
       { icon: 'github', link: 'https://github.com/hua545563233' }
@@ -28,7 +67,7 @@ export default defineConfig({
 
     footer: {
       message: 'Released under the MIT License.',
-      copyright: 'Copyright © 2019-present liu zhihua'
+      copyright: 'Copyright © 2023-present liu zhihua'
     }
   }
 })
--- docs/index.md
+++ docs/index.md
@@ -3,23 +3,23 @@
 layout: home
 
 hero:
-  name: "Frontend_About"
-  text: "Frontend1 "
-  tagline: My great project tagline
+  name: "Frontend About"
+  text: "All About Frontend "
+  tagline: 技术文章、书籍分享、面试真题、日常随笔
   actions:
     - theme: brand
-      text: Markdown Examples
-      link: /markdown-examples
-    - theme: alt
-      text: API Examples
-      link: /api-examples
+      text: 开始探索
+      link: /frontend/html
 
 features:
-  - title: 覆盖全面
-    details: 涵盖大前端的全部核心知识，包含 HTML、CSS、JavaScript 、MVVM 框架、ReactNative、Flutter、NodeJs、前端工程化、 数据结构与算法JavaScript实现等。
-  - title: 内容深度
+  - title: Widely
+    icon: 🚀
+    details: 涵盖大前端的全部核心知识，包含 HTML、CSS、JavaScript 、MVVM 框架、React、NodeJs、前端工程化、 数据结构与算法JavaScript实现等。
+  - title: Deeply
+    icon: 📚
     details: 深入JavaScript 运行机制、浏览器渲染原理、 MVVM 框架源码分析、掌握设计模式与编程思想。将持续保持更新。
-  - title: 实践结合面试
+  - title: Useful
+    icon: 📝
     details: 核心知识与日常实践、面试题相结合，既可备考面试也能在日常开发中参考。
 ---
 
